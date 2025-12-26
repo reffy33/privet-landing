c4d292f78ecad80cdddf7bdc41976e7229ea66b7
reffy33 <reffymin@gmail.com>
2025-12-27 01:37:28 +0500

    Добавлено бургер меню

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,48 @@
 import { NavLink } from "react-router";
-// import ru_flag from "../assets/ru_flag.svg";
+import burger from "../assets/burger.svg";
+import { useState } from "react";
+import cn from "../utils/tailwindMerge";
 
 export default function NavBar() {
+  const [burgerOpen, setBurgerOpen] = useState(false);
+
+  const handleBurgerClick = () => setBurgerOpen(!burgerOpen);
+
   return (
-    <nav className="bg-white">
-      <div className="container">
-        <ul className="flex gap-10 justify-end items-center text-black-text py-8 max-xl:py-6 max-md:py-4">
-          <li>
-            <NavLink to="/">Главная</NavLink>
-          </li>
-          <li>
-            <NavLink to="/to_students">Студентам</NavLink>
-          </li>
-          <li>
-            <NavLink to="/to_universities">Вузам</NavLink>
-          </li>
-          <li>
-            <a href="https://privet-web-steel.vercel.app/">Веб-приложение ↗</a>
-          </li>
-          {/* <li>
-            <div className="border-2 border-primary-yellow rounded-[10px] p-1.75">
-              <img src={ru_flag} alt="" className="h-[44.8px] w-[44.8px]" />
-            </div>
-          </li> */}
-        </ul>
+    <>
+      <nav className="bg-white relative z-50">
+        <div className="container">
+          <ul className="flex gap-10 justify-end items-center text-black-text py-8 max-xl:py-6 max-md:py-4">
+            <li>
+              <NavLink to="/">Главная</NavLink>
+            </li>
+            <li>
+              <NavLink to="/to_students">Студентам</NavLink>
+            </li>
+            <li>
+              <NavLink to="/to_universities">Вузам</NavLink>
+            </li>
+            <li className="max-lg:hidden">
+              <a href="https://privet-web-steel.vercel.app/">
+                Веб-приложение ↗
+              </a>
+            </li>
+            <li className="lg:hidden">
+              <div className="cursor-pointer" onClick={handleBurgerClick}>
+                <img src={burger} alt="" />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div
+        className={cn(
+          "pb-2 pr-5 flex justify-end transition-ease duration-350 absolute bg-white w-screen z-10 top-14 opacity-0",
+          burgerOpen ? "opacity-100" : ""
+        )}
+      >
+        <a href="https://privet-web-steel.vercel.app/">Веб-приложение ↗</a>
       </div>
-    </nav>
+    </>
   );
 }
